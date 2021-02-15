bb951c1131327f4124b8290ee8009e38a29afd0e
Chad Lefort <chadlefort@gmail.com>
2021-02-15 16:14:33 -0600

    Add other routes for contacts playground app

--- mfe/contacts/src/app/App.tsx
+++ mfe/contacts/src/app/App.tsx
@@ -3,8 +3,11 @@ import { ContactType } from '@fake-company/types';
 import { Box } from '@material-ui/core';
 import React, { FC } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
+import { AddContacts } from '../feature-core/components/AddContacts';
+import { EditContact } from '../feature-core/components/EditContact';
+import { ViewContact } from '../feature-core/components/ViewContact';
 import { ViewContacts } from '../feature-core/components/ViewContacts';
 import { store } from './reducer';
 
@@ -13,7 +16,12 @@ export const App: FC = () => (
     <Theme>
       <Router>
         <Box p={3}>
-          <ViewContacts type={[ContactType.Customer]} />
+          <Switch>
+            <Route path="/edit/:id" render={() => <EditContact type={[ContactType.Customer]} />} />
+            <Route path="/add" render={() => <AddContacts type={[ContactType.Customer]} />} />
+            <Route path="/:id" render={() => <ViewContact type={[ContactType.Customer]} />} />
+            <Route path="/" render={() => <ViewContacts type={[ContactType.Customer]} />} />
+          </Switch>
         </Box>
       </Router>
     </Theme>
